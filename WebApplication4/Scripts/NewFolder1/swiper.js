6ec33de37247b75ba107f2f7e67795860fe61bd0
SV <SV@PM19-PC06>
2024-11-02 10:08:28 +0700

    Add project files.

---
+++ WebApplication4/Scripts/NewFolder1/swiper.js
@@ -0,0 +1,8 @@
+﻿var swiper = new Swiper(".swiper", {
+    slidesPerView: 'auto',
+    spaceBetween: 10,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    }
+});
